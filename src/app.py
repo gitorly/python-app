194a0df8d3746c56cc833fe13e3c3f3bab67f124
Orly <jdevincenzo8@gmail.com>
2026-06-19 10:38:04 -0300

    Changes to app.py and created Dockerfile

--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
+import datetime
+import socket
 
 app = Flask(__name__)
 
 @app.route('/api/v1/details')
 
 def details():
-    return 'Hello World'
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B %d, %Y"),
+        'hostname': socket.gethostname()
+    }) 
 
-if __name__ == __main__:
+if __name__ == '__main__':
     app.run()
